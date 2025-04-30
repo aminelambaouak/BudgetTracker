ffa2c3e2bd3e934852cf32836cef52575704ec6a
aminelambaouak <aminelambaouak@gmail.com>
2025-04-30 20:55:13 +0100

    changes

--- app.py
+++ app.py
@@ -1,65 +1,55 @@
 #Budget Tracker Application
 
 class Transaction:
-    def __init__(self, budget = None):
+    def __init__(self,  budget = None):
         self.budget = budget
         self.listIncomes = []
-        self.expensesDict = {}
-
+        self.expensesList = []
     #ask for incomes
     def incomes(self):  
         numberIncomes = 0
-        print('press 0 to exit')
-        while self.budget != 0:
-            try:
-                numberIncomes += 1
-                self.budget = float(input('what is the amount income : '))
-                print('-'* 20)
-                self.listIncomes.append(self.budget)
+        print("Welcome to Budget Tracker")
+        print('-'*50)
+        print('press 0 when no source to add')
+        print('-'*50)
+        while True:
+            if self.budget != 0:
                 
-            except ValueError:
-                print('Please enter a number')
- 
-        budgetSum = sum(self.listIncomes)
-        
-        return f'the sum of your incomes {budgetSum}'
-    
+                try:
+                    numberIncomes += 1
+                    self.budget = float(input('what is the amount income : '))
+                    self.listIncomes.append(self.budget)
+                    
+                except ValueError:
+                    print('Please enter a number')
+            else:
+                break
+        print(self)
+    # Expenses function to calculate the sum of user's expenses
     def expenses(self):
-        print("""           
-                    1 : Rent/Mortgage
-                    2 : Food/Groceries
-                    3 : Utilities
-                    4 : Transportation
-                    5 : Healthcare
-                    6 : Debt Payments
-                    7 : Personal Care
-                    8 : Internet/Phone""")
-        print('press enter to exit')
-        expensesDict = {
-            1 : 'Rent/Mortgage', 2 : 'Food/Groceries', 3 : 'Utilities', 4 : 'Transportation', 5 : 'Healthcare', 
-                        6 : 'Debt Payments', 7 : 'Personal Care', 8 : 'Internet/Phone'
-        }
-        try:
-            for item in range(len(expensesDict)):            
-                expensesName, expensesAmount = input('what is your expense : '), float(input('what is the amount of the expense : '))
- 
-        except ValueError as e:
-            print('Please enter a number between 1 and 8')
-            if expensesName not in range(8):
-                raise(e)
-        return self.expensesDict
+        #Tell the user to press 0 to exit
+        while True:
+            try:
+                expenseCategory = input('what is the expense : ')
+                expenseAmount = float(input("Enter income (0 to stop): "))
+                if expenseAmount == 0:
+                    break
+                elif expenseAmount < 0:
+                    print("Please enter a positive number.")
+                else:
+                    self.expensesList.append(expenseAmount)
+            except ValueError:
+                print('Please enter a real number')
+        print(self)
         
-    
-    #add budget
-    def addBudget(self):
-        pass
-    
-    
-    
-    
-    #view budget
-    def viewBudget(self):
-        pass
+    def getBalance(self):
+        balance = self.incomes() - self.expenses()
+        return balance
+    # print a string 
+    def __str__(self):
+         return f'The sum of your expenses is : {sum(self.expensesList):.2f}, total income : {sum(self.listIncomes):.2f} and the balace : {self.getBalance():.2f}'
+
     
 a = Transaction()
+print(a.incomes())
 print(a.expenses())
