7a8d4d811015e6ef67bb5a06c3dcf3ee1522eca5
aminelambaouak <aminelambaouak@gmail.com>
2025-05-01 16:10:58 +0100

    modifications

--- scripts/budget_tracker.py
+++ scripts/budget_tracker.py
@@ -1,55 +1,82 @@
 #Budget Tracker Application
+import pandas as pd
 
-class Transaction:
-    def __init__(self,  budget = None):
+class BudgetTracker:
+    def __init__(self,  budget= None):
         self.budget = budget
-        self.listIncomes = []
-        self.expensesList = []
+        self.expensesCategories = []
+
+        self.expensesDict = {}
+        self.AmountsDict = {}
     #ask for incomes
     def incomes(self):  
-        numberIncomes = 0
         print("Welcome to Budget Tracker")
         print('-'*50)
         print('press 0 when no source to add')
         print('-'*50)
         while True:
-            if self.budget != 0:
-                
-                try:
-                    numberIncomes += 1
-                    self.budget = float(input('what is the amount income : '))
-                    self.listIncomes.append(self.budget)
-                    
-                except ValueError:
-                    print('Please enter a number')
-            else:
-                break
+            try: 
+                self.Income = input('what is your income : ')  
+                self.budget = float(input('what is the income amount (0 to stop): '))
+                if self.budget == 0:
+                    break
+                elif self.budget < 0:
+                    print("Please enter a positive number.")
+                else:
+
+                    if self.Income in self.AmountsDict:
+                        self.AmountsDict[self.Income] += self.budget
+                    else:
+                        self.AmountsDict[self.Income] = self.budget
+            except ValueError:
+                print('Please enter a number')
+
         print(self)
     # Expenses function to calculate the sum of user's expenses
     def expenses(self):
         #Tell the user to press 0 to exit
         while True:
             try:
-                expenseCategory = input('what is the expense : ')
-                expenseAmount = float(input("Enter income (0 to stop): "))
+                expenseCategory = input('what is the expense category: ')
+                expenseAmount = float(input("Enter expense amount (0 to stop): "))
                 if expenseAmount == 0:
                     break
                 elif expenseAmount < 0:
                     print("Please enter a positive number.")
                 else:
-                    self.expensesList.append(expenseAmount)
+
+                    if expenseCategory in self.expensesDict:
+                        self.expensesDict[expenseCategory] += expenseAmount
+                    else:
+                        self.expensesDict[expenseCategory] = expenseAmount
             except ValueError:
                 print('Please enter a real number')
         print(self)
-        
+    # calculate the balance
     def getBalance(self):
-        balance = sum(self.listIncomes) - sum(self.expensesList)
+        balance = sum(self.AmountsDict.values()) - sum(self.expensesDict.values())
         return balance
     # print a string 
     def __str__(self):
-         return f'The sum of your expenses is : {sum(self.expensesList):.2f}, total income : {sum(self.listIncomes):.2f} and the balance : {self.getBalance():.2f}'
+        if sum(self.expensesDict.values()) == 0:
+            return f'total income : {sum(self.AmountsDict.values()):.2f}'
+        elif sum(self.AmountsDict.values()) == 0:
+            return f'total expenses : {sum(self.expensesDict.values()):.2f} and balance : {self.getBalance():.2f}'
+        else:
+            return f""" 
+                        Incomes : {self.AmountsDict}
+                        total income : {sum(self.AmountsDict.values()):.2f}
+                        expenses : {self.expensesDict}
+                        total expenses :  {sum(self.expensesDict.values()):.2f}
+                        balance : {self.getBalance():.2f}"""
+                    
+    #def extractData(self):
+                
+        
+     
 
     
-a = Transaction()
+a = BudgetTracker()
 print(a.incomes())
 print(a.expenses())
+#print(a.extractData())
