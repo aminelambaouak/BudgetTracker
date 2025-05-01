9c5ab71f4c00d2c21b65129d2f5c06030af0664c
aminelambaouak <aminelambaouak@gmail.com>
2025-05-01 23:52:51 +0100

    modifications

--- scripts/budget_tracker.py
+++ scripts/budget_tracker.py
@@ -1,12 +1,15 @@
 #Budget Tracker Application
-import pandas as pd
 import json
+import sqlite3
+import mysql.connector
 
 class BudgetTracker:
     def __init__(self,  budget= None):
         self.budget = budget
         self.expensesDict = {}
         self.AmountsDict = {}
+        self.data = {}
+        
     #ask for incomes
     def incomes(self):  
         print("Welcome to Budget Tracker")
@@ -15,20 +18,24 @@ class BudgetTracker:
         print('-'*50)
         while True:
             try: 
-                self.Income = input('what is your income : ')  
-                self.budget = float(input('what is the income amount (0 to stop): '))
-                if self.budget == 0:
+                incomeCategory = input('income category : ')  
+                budgetAmount = float(input('what is the income amount (0 to stop): '))
+                if budgetAmount == 0:
                     break
-                elif self.budget < 0:
+                elif budgetAmount < 0:
                     print("Please enter a positive number.")
                 else:
 
-                    if self.Income in self.AmountsDict:
-                        self.AmountsDict[self.Income] += self.budget
+                    if incomeCategory in self.AmountsDict:
+                        self.AmountsDict[incomeCategory] += budgetAmount
                     else:
-                        self.AmountsDict[self.Income] = self.budget
+                        self.AmountsDict[incomeCategory] = budgetAmount
             except ValueError:
                 print('Please enter a number')
+            if incomeCategory.isdigit():
+                print("Error: Please enter text, not a number for income category.")
+            else:
+                continue
 
         print(self)
     # Expenses function to calculate the sum of user's expenses
@@ -36,7 +43,7 @@ class BudgetTracker:
         #Tell the user to press 0 to exit
         while True:
             try:
-                expenseCategory = input('what is the expense category: ')
+                expenseCategory = input('expense category: ')
                 expenseAmount = float(input("Enter expense amount (0 to stop): "))
                 if expenseAmount == 0:
                     break
@@ -50,6 +57,12 @@ class BudgetTracker:
                         self.expensesDict[expenseCategory] = expenseAmount
             except ValueError:
                 print('Please enter a real number')
+                
+            if expenseCategory.isdigit():
+                print("Error: Please enter text, not a number for expense category.")
+            else:
+                continue
+
         print(self)
     # calculate the balance
     def getBalance(self):
@@ -72,7 +85,8 @@ class BudgetTracker:
     def extractData(self):
         data = {
             "income": self.AmountsDict,
-            "expenses": self.expensesDict
+            "expenses": self.expensesDict,
+            "balance" : self.getBalance()
         }
         with open("budget_data.json", "w") as file:
             json.dump(data, file)
@@ -80,10 +94,69 @@ class BudgetTracker:
         with open("budget_data.json", "r") as file:
             loaded_data = json.load(file)
             print('Your data = ', loaded_data)
-     
+    
+    def database(self):
+        # Connect to database (it will create one if it doesn't exist)
+        mydb = mysql.connector.connect(
+                        host="localhost",
+                        user="root",
+                        passwd="070193",
+                        database = 'BudgetTracker'
+                        )
+        myCursor = mydb.cursor()
 
+        # Create the new database if it does not exist
+        myCursor.execute("CREATE DATABASE IF NOT EXISTS BudgetTracker")
+
+        # Confirm that the database was created by listing the databases
+        myCursor.execute("SHOW DATABASES")
+
+        # Fetch and print the databases
+        for db in myCursor:
+            print(db)
+
+        # Create a cursor object to execute SQL commands
+
+        
+        myCursor.execute('''CREATE TABLE IF NOT EXISTS my_balances (  income_category VARCHAR(20),
+                                                                    income_amount REAL,
+                                                                    expense_category VARCHAR(20),
+                                                                    expense_amount REAL);''')
+        
+
+
+            # Insert income entries (with NULL for expense columns)
+        for category, amount in self.AmountsDict.items():
+            myCursor.execute(
+                "INSERT INTO my_balances (income_category, income_amount, expense_category, expense_amount) VALUES (?, ?, ?, ?)",
+                (category, amount, None, None);
+            )
+
+        # Insert expense entries (with NULL for income columns)
+        for category, amount in self.expensesDict.items():
+            myCursor.execute(
+                "INSERT INTO my_balances (income_category, income_amount, expense_category, expense_amount) VALUES (?, ?, ?, ?)",
+                (None, None, category, amount)
+            )
+        
+        myCursor.execute('''SELECT * FROM my_balances;''')
+        rows = myCursor.fetchall()
+
+        for row in rows:
+            print(row)
+            
+        myCursor.commit()
+
+        # Close connection
+        myCursor.close()
+        
+        
+        
+        
+        
     
 a = BudgetTracker()
 print(a.incomes())
 print(a.expenses())
 print(a.extractData())
+#print(a.database())
